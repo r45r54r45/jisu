de48e7afdbbe8ce6eb4cd5b161b4cdc600387552
Unknown <jisujenlee@gmail.com>
2017-06-03 21:13:24 +0900

    Auto stash before merge of "movit-local" and "movit/master"
    views_feed

---
+++ views_feed/index.php
@@ -0,0 +1,213 @@
+<?php
+include "../views_common/header_with_login.php"
+?>
+
+
+
+
+
+<!--profile-->
+<div class = "container">
+	<div class="section">
+
+		<div class="row">
+		  <div class="col s12">
+		    <div class="row">
+		      <div class="col s12">
+		        <div class="card">
+		          <div class="card-image">
+		            <img src="http://www.img.lirent.net/2014/10/Android-Lollipop-wallpapers-Download.jpg" alt="" / style="height: 200px">
+		          </div>
+		          <div class="card-content">
+		            <div class="row">
+		              <div class="col s4 profile-pic">
+		                <img class="circle responsive-img" src="https://lh3.googleusercontent.com/-W2XryVdi-lA/U6tSAh3SsbI/AAAAAAAAFGY/ZHJiUEcR_Zs/w480-h480/avatar%2Bmaterial%2Bdesign.png" alt="" />
+		              </div>
+		              
+		            </div>
+		            <span class="card-title black-text">Bruce Banner Wayne</span>
+		          </div>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+		</div>		
+
+	</div>
+</div>
+
+
+<!--gallery-->
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Card Section   -->
+      <div class="row">
+
+      	<!-- Card 1 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+		
+		<!-- Card 2 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+		
+		<!-- Card 3 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+
+
+      </div>
+
+
+      <!--   Card Section 2   -->
+      <div class="row">
+
+      	<!-- Card 1 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+		
+		<!-- Card 2 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+		
+		<!-- Card 3 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+
+
+      </div>      
+      <!--   Card Section 3   -->
+      <div class="row">
+
+      	<!-- Card 1 // 이 모양으로 하나씩 추가 -->
+        <div class="col s12 m4">
+		  <div class="card">
+		    <div class="card-image waves-effect waves-block waves-light">
+		      <img class="activator" src="/background1.jpg">
+		    </div>
+		    <div class="card-content">
+		      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+		      <p><a href="#">This is a link</a></p>
+		    </div>
+		    <div class="card-reveal">
+		      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+		      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+		    </div>
+		  </div>
+        </div>
+		
+
+
+
+      </div>      
+    </div>
+  </div>
+
+
+
+<!--글쓰기 FAB-->
+<?php
+include "../views_common/fixed_action_btn.php"
+?>
+
+<!--footer-->
+
+<?php
+include "../views_common/footer.php"
+?>
+
+<script type="text/javascript" src="../js/materialize.min.js"></script>
+<script src="../js/init.js"></script>
+<script>
+    $(document).ready(function(){
+    //init
+    $('.modal').modal();
+    $('.carousel').carousel();
+    $('.slider').slider({full_width: true, interval: 3000});
+    });
+    $(".button-collapse").sideNav();
+    $('.chips').material_chip();
+    $('.parallx').parallx();
+
+
+</script>
