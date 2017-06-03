108ba09842208860dfb19c9d8d9a662d0d3b5205
DESKTOP-ANHLNBR\iz000 <yunhojang93@gmail.com>
2017-06-03 15:49:57 +0900

    fixed_action_btn파일 따로 뺌

--- views_movieinfo/index.php
+++ views_movieinfo/index.php
@@ -20,6 +20,7 @@ if(!isset($result_item)){
   exit();
 }
 else{
+  //이미지와 줄거리 파싱해오는 부분
   $html = file_get_html($result_item['link']);
   foreach($html->find(".con_tx") as $text){
     $result_item['story'] = $text->plaintext;
@@ -94,66 +95,6 @@ else{
 
 <?php
 include_once ("../views_common/fixed_action_btn.php");
-?>
-
-<!-- Modal Structure (Posting)-->
-
-        <div id="modal3" class="modal modal-fixed-footer">
-            <div class = "modal-content">
-                <h4>Posting</h4>
-                <div class="row">
-                <form class="col s12">
-
-                     <div class="file-field input-field">
-                      <div class="btn">
-                        <span>File</span>
-                        <input type="file">
-                      </div>
-                      <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text">
-                      </div>
-                    </div>                   
-                                      
-
-
-                 
-
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <i class="material-icons prefix">mode_edit</i>
-                            <textarea id="textarea" class="materialize-textarea"></textarea>
-                            <label for="textarea">Write Text</label>
-                        </div>
-                    </div>
-
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                          <i class="material-icons prefix">label</i>
-                          <div id="tags" class="chips"></div>
-                          <label for="tags">Write tags and hit enter</label>
-
-                        </div>
-                      
-
-                    </div>
-      
-
-                </form>
-                </div>
-            </div>
-
-            <div class="modal-footer">
-                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                  <i class="material-icons right">send</i>
-                </button>               
-            </div>
-        </div>
-
-
-
-<?php
 include "../views_common/footer.php";
 ?>
 
